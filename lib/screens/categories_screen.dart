bdf3699dabba26d168968139cd01be958319ecaa
mehar <arunillapu@gmail.com>
2024-10-21 11:34:47 +0530

    screens added

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+class cat extends StatelessWidget {
+  const cat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Categories',style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+      ),
+    );
+  }
+}
